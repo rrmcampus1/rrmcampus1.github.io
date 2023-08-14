5175d70616b67a8c2db7efb43357ed2889c2784a
amreshthakur1 <120268841+amreshthakur1@users.noreply.github.com>
2023-08-14 10:36:12 +0545

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,30 @@
+
+
+function myFunction() {
+    var y = document.getElementById("mytop");
+    if (y.className === "topnav") {
+        y.className += " responsive";
+    } else {
+        y.className = "topnav";
+    }
+};
+function myFunction1() {
+    var y = document.getElementById("my");
+    if (y.className === "topnav") {
+        y.className += " responsive";
+    } else {
+        y.className = "topnav";
+    }
+};
+
+function myFunction2() {
+    var y = document.getElementById("mynav");
+    if (y.className === "topnav") {
+        y.className += " responsive";
+    } else {
+        y.className = "topnav";
+    }
+};
+
+
+
