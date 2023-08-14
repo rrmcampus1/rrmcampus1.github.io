b5c85b6ae3c527928f14df25af462691136b17db
amreshthakur1 <120268841+amreshthakur1@users.noreply.github.com>
2023-08-14 10:34:11 +0545

    Delete javascript.js

--- javascript.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-    var y = document.getElementById("myTopnav");
-    if (y.className === "topnav") {
-        y.className += " responsive";
-    } else {
-        y.className = "topnav";
-    }
-};
-
